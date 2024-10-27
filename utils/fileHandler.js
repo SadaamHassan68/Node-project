68c14a9cc08f2bf1476dd8ddc048d38654a30e22
sadaamhassan688 <sadamhassan688@gmail.com>
2024-10-27 02:42:41 -0800

    first commit

---
+++ utils/fileHandler.js
@@ -0,0 +1,17 @@
+const fs=require('fs');
+exports.writeTasksToFile=(data)=>{
+    
+    fs.writeFileSync('./../data/tasks.json',data)
+    return data;
+}
+exports.readTaskToFile =()=>{
+    if(fs.existsSync('./../data/tasks.json')){
+        this.writeTasksToFile([])
+
+    }
+    const data=fs.readFileSync(('./../data/tasks.json'))
+        return JSON.parse(data);
+
+    
+   
+}
