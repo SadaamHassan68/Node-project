81f013f9d2fb8ae8214b61e36b8d736326ca571a
sadaamhassan688 <sadamhassan688@gmail.com>
2024-10-28 07:59:03 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const { log } = require('console');
+const http = require('http');
+const { json } = require('stream/consumers');
+const PORT =9000;
+const HOSTNAME='localhost';
+
+const server=http.createServer( (req,res)=>{
+    if(req.url.startsWith('./task')){
+        //Todo:Implement this method
+    }
+    else{
+        res.writable(404,'not found',{'count-type':'application/js'})
+        res.end(json.stringify({
+            message:'PAGE '
+        }))
+    }
+});
+
+server.listen(PORT,HOSTNAME,()=>{
+    console.log('server running at ${PORT');
+    
+
+
+})
---
+++ controllers/taskControllers.js
@@ -0,0 +1,43 @@
+const {IncomingForm }=require('formidable')
+const { readTaskToFile, writeTasksToFile } = require("../utils/fileHandler")
+
+exports.getTasks=(req,res)=>{
+    const tasks=readTaskToFile();
+    res.writeHead(200,{'content-type':'application/json'});
+    res.end(JSON.stringify(tasks))
+
+}
+exports.createTask=(req,res)=>{
+    const form=new IncomingForm();
+    form.parse(req,(err,fields,files)=>{
+        if(err){
+            res.writeHead(400,{'content-type':'application/json'})
+            res.end(JSON.stringify({
+                message:'error parsing form'
+            }))
+            return;
+            
+        }
+        const tasks=readTasksFromFile()
+        const NewTasks={
+            id: Date.now(),
+            title:fields.title,
+            description:fields?.description || '',
+            status:fields?.status || 'pending',
+            Image:fields.Image? `/uploads/${files.Image.name}`: null,
+
+        }
+        tasks.push(NewTasks);
+        
+        writeTasksToFile(tasks);
+         if(files.Image){
+            copyFileSync(files.Image.path,path.join(__dirname,'../uploads',  files.Image.name));
+            res.end(JSON.stringify(NewTasks))
+
+         }
+    })
+
+}
+
+
+
