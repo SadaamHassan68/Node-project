33ad3ab8c0ec7cd3ed83af318e77df339e740153
sadaamhassan688 <sadamhassan688@gmail.com>
2024-11-02 08:15:35 -0800

    commit

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,27 +1,20 @@
-const { getTasks ,createTask,upsateTasks,deleteTask } = require("../controllers/taskControllers")
+const { getTasks, createTask, updateTask, deleteTask } = require("../controllers/taskControllers");
 
-const taskRoutes = (req,res)=>{
-    if(req.method ==='GET'){
-        getTasks(req,res)
+const taskRoutes = (req, res) => {
+    if (req.method === 'GET') {
+        getTasks(req, res);
+    } else if (req.method === 'POST') {
+        createTask(req, res);
+    } else if (req.method === 'PATCH') {
+        updateTask(req, res);
+    } else if (req.method === 'DELETE') {
+        deleteTask(req, res);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({
+            message: 'PAGE not found'
+        }));
     }
-    else if (req.method ==='POST'){
-        createTask(req,res)
+};
 
-    }
-    else if(req.method ==='PATCH'){
-        upsateTasks(req,res)
-    }
-    else if(req.method ==='DELETE'){
-       deleteTask(req.res)
-    }
-    else
-    {
-        res.writeHead(404,'Not found',{'content-type':'application/js'})
-        res.end(json.stringify({
-            message:'PAGE not found'
-        }))
-    }
-
-
-}
-module.exports =taskRoutes
+module.exports = taskRoutes;
