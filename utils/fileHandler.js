053c48e4f58733141c891d2daadf2d325a72304e
sadaamhassan688 <sadamhassan688@gmail.com>
2024-11-02 08:14:09 -0800

    commit

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -1,18 +1,32 @@
-//this is file handler
-const fs=require('fs');
-exports.writeTasksToFile=(data)=>{
-    
-    fs.writeFileSync('./../data/tasks.json',data)
-    return data;
-}
-exports.readTaskToFile =()=>{
-    if(fs.existsSync('./../data/tasks.json')){
-        this.writeTasksToFile([])
+const fs = require('fs');
+const path = require('path');
 
+
+const filePath = path.join(__dirname, '../data/tasks.json');
+
+
+exports.writeTasksToFile = (data) => {
+    try {
+        fs.writeFileSync(filePath, JSON.stringify(data, null, 2)); 
+        return data;
+    } catch (error) {
+        console.error('Error writing to file:', error);
+        throw error; 
     }
-    const data=fs.readFileSync(('./../data/tasks.json'))
-        return JSON.parse(data);
+};
 
-    
+
+exports.readTasksFromFile = () => {
    
-}
+    if (!fs.existsSync(filePath)) {
+        exports.writeTasksToFile([]);  
+    }
+    
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');  
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error reading from file:', error);
+        throw error;  
+    }
+};
